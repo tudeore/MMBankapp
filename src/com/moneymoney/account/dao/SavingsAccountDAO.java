0e4584e4a387f70e4f00b79f6da7d2039b86bcb1
Deore <=>
2018-12-29 18:30:38 +0530

    adds MMBank assignment

---
+++ src/com/moneymoney/account/dao/SavingsAccountDAO.java
@@ -0,0 +1,26 @@
+package com.moneymoney.account.dao;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import com.moneymoney.account.SavingsAccount;
+import com.moneymoney.exception.AccountNotFoundException;
+
+public interface SavingsAccountDAO {
+	
+	SavingsAccount createNewAccount(SavingsAccount account) throws ClassNotFoundException, SQLException;
+
+	SavingsAccount getAccountById(int accountNumber) throws ClassNotFoundException, SQLException, AccountNotFoundException;
+	SavingsAccount deleteAccount(int accountNumber) throws ClassNotFoundException, SQLException;
+	List<SavingsAccount> getAllSavingsAccount() throws ClassNotFoundException, SQLException;
+	void updateBalance(int accountNumber, double currentBalance) throws ClassNotFoundException, SQLException;
+	void commit() throws SQLException;
+	SavingsAccount updateAccount(int accountNumber, int choice, String name) throws SQLException, ClassNotFoundException;
+
+	SavingsAccount serachAccount(String name, int choice) throws ClassNotFoundException, SQLException;
+
+	List<SavingsAccount> searchAccountBySalary(int minSalary, int maxSalary) throws SQLException, ClassNotFoundException;
+
+	
+	
+}
